00877b3928f29acbd9428efeb6d0af2ca18b39e5
EuroV8 <yeboy134@gmail.com>
2024-04-01 18:55:12 +0200

    Update rapport & Makefile; Add /bin/ and /scripts/

---
+++ scripts/run_L2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+
+RESULTS_PATH=../maqao_html/
+GCC_O1_BIN=../bin/measure_gcc_O1
+GCC_O2_BIN=../bin/measure_gcc_O2
+GCC_O3_BIN=../bin/measure_gcc_O3
+
+maqao oneview -R1 -uarch=ZEN_V1 -xp=../maqao_html/
