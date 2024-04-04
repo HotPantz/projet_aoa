f02f217c043483249d86990bb1cda2da1d73188c
EuroV8 <yeboy134@gmail.com>
2024-04-04 21:52:05 +0200

    Update rapport + fix typo in measure_amd.sh

--- scripts/measures_amd.sh
+++ scripts/measures_amd.sh
@@ -8,7 +8,7 @@ size=300
 RESULTS_DIR="../results/no_opt"
 FREQ="2.4GHz"
 
-commands=("measure_gcc_NOOPT" "measure_O3_gcc_NOOPT" "measure_OFast_gcc_NOOPT" "measure_O3NATIVE_gcc_NOOPT" "measure_AVX2_gcc_NOOPT" "measure_OCUSTOM_gcc_NOOPT" "measure_AOCC_NOOPT")
+commands=("measure_gcc_NOOPT" "measure_O3_gcc_NOOPT" "measure_OFast_gcc_NOOPT" "measure_O3NATIVE_gcc_NOOPT" "measure_AVX2_gcc_NOOPT" "measure_OCUSTOM_gcc_NOOPT" "measure_AOCC_NOOPT" "measure_O3_AOCC_NOOPT)
 
 sudo cpupower -c 1 frequency-set -f $FREQ
 
