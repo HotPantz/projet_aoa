a19573711541bfe47ed697d0c6d2fe399f5e8119
Sami Ouraghene <zouraghene@gmail.com>
2024-04-22 16:23:51 +0200

    Permutation

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,13 @@
-#ifdef OPT1
-/* OPT code 1 */
+#ifdef PERMUTATION
+void kernel (unsigned n, double x[n], const double y[n], const double z[n][n]) {
+   unsigned i, j;
+
+   for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            x[i] += z[i][j] / y[i];
+        }
+   }
+}
 
 #elif defined OPT2
 
