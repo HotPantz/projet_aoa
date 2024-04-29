ba75d1efdee8f666dee0db0cebf79f95732597c9
HotPantz <118857477+HotPantz@users.noreply.github.com>
2024-04-29 16:55:19 +0200

    DIVOPTI2 meilleur version avec permu, simple precision , division simplifié

--- src/kernel.c
+++ src/kernel.c
@@ -41,16 +41,16 @@ void kernel (unsigned n, double x[n], const double y[n], const double z[n][n]) {
 
 #elif defined DIVOPTI2
 
-void kernel (unsigned n, double x[n], double y[n], const double z[n][n]) {
+void kernel (unsigned n, float x[n], float y[n], float z[n][n]) {
    unsigned i, j;
 
    for (i = 0; i < n; i++) {
-        y[1] = 1 / y[i];
+        y[i] = 1 / y[i];
     }
 
    for (i = 0; i < n; i++) {
         for (j = 0; j < n; j++) {
-            x[i] += z[i][j] * y[i];
+            x[i] += z[i][j] / y[i];
         }
    }
 }
