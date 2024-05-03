4378df66eecbc9f1093afa7c3017447d0787d551
EuroV8 <yeboy134@gmail.com>
2024-05-03 23:52:00 +0200

    fix maqao.sh

--- bin/maqao.sh
+++ bin/maqao.sh
@@ -10,13 +10,14 @@ fi
 opti_type=$1
 
 # Vérifier si un exécutable avec le nom spécifié est présent
-if [ ! -x "./measure_gcc_$opti_type" ]; then
+if [ ! -x "./measure_phase2_$opti_type" ]; then
     echo "Aucun exécutable avec le nom spécifié n'est présent dans ce répertoire."
     exit 1
 fi
 
 # Lancer la commande avec maqao
-maqao OV -R1 -xp="$opti_type" -uarch=ZEN_V1 -- "./measure_gcc_$opti_type" 700 30 20
+maqao OV --replace -R1 -xp="$opti_type" -uarch=ZEN_V1 -- "./measure_phase2_$opti_type" 300 18 1750
 
 # Déplacer le répertoire opti_one_html dans le dossier bin avec le nom de l'optimisation spécifié
-mv /home/hotpantz/Documents/projet_aoa/bin/"$opti_type"/RESULTS/measure_gcc_"$opti_type"_one_html /home/hotpantz/Documents/projet_aoa/bin/"$opti_type"_one_html
+#rm -r ./measure_phase2_"$opti_type"_one_html
+mv -f ./"$opti_type"/RESULTS/measure_phase2_"$opti_type"_one_html .
